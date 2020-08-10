073d1fc8cc6dc5643f6359bb5e2425f073603468
Tamim Abo Tafish <tamimabotafish@gmail.com>
2020-08-10 19:14:43 +0200

    adding gitignore file

--- cypress/integration/and.js
+++ cypress/integration/and.js
@@ -1,19 +1,19 @@
-//// <reference types="Cypress" />
+/// <reference types="Cypress" />
 
 
-// describe('testing feacbook website', () =>{
+describe('testing feacbook website', () =>{
     
-//     it('open feacbook', ()=>{
+    it('open feacbook', ()=>{
 
-//         cy.viewport('macbook-15');
-//         cy.visit('https://facebook.com');
-//         cy.get('button').should('have.class','active').and('not.be.visible');
+        cy.viewport('macbook-15');
+        cy.visit('https://facebook.com');
+        cy.get('a').should('contain','Sign Up').and('have.attr', 'href').and('match', /r.php/).and('not.include','#');
         
 
 
 
-//     })
+    })
 
 
 
-//})
+ })
